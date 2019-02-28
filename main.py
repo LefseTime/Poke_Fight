8f8bf3d140866facde41bb3c6b8671059047a550
LefseTime <scfunkenbusch@gmail.com>
2019-02-28 13:10:30 -0600

    poke selection

--- main.py
+++ main.py
@@ -8,17 +8,18 @@ valid = False
 type = "error"
 while not valid:
     type_num = input("------------\n   Pokes  \n------------\n  1. SquirtGun\n  2. CharMangler\n  3. BulbousSore\n  4. MagiKrap\n------------\nWhich Poke do you choose? ")
-    if type_num == '1' or "SquirtGun":
+    if type_num == "1" or type_num == "SquirtGun":
         type = "SquirtGun"
         valid = True
-    elif type_num == '2' or "CharMangler":
+    elif type_num == "2" or type_num == "CharMangler":
         type = "CharMangler"
         valid = True
-    elif type_num == '3' or "BulbousSore":
+    elif type_num == "3" or type_num == "BulbousSore":
         type = 'BulbousSore'
         valid = True
-    elif type_num == '4' or "MagiKrap":
+    elif type_num == "4" or type_num == "MagiKrap":
         type = 'MagiKrap'
         valid = True
 
-print(type)
+print("\nYour new {} looks up at you adoringly.\nIn the distance, a wild MulletEagle screeches, and a single tear rolls down your cheek.".format(type))
+name = str(input("\nWhat would you like to name your precious {}? ".format(type)))
